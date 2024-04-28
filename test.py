5a2fb13d95dd1775a989ed734b4aa0c8936bb1c9
Bridget Gorman <birdget22@gmail.com>
2024-04-28 14:35:15 -0500

    spacing

--- test.py
+++ test.py
@@ -1,20 +1,21 @@
-  vals = [
-        158,
-        146,
-        144,
-        147,
-        238,
-        243,
-        236,
-        240,
-        239,
-        232,
-        237,
-        228
-   ]
+vals = [
+    158,
+    146,
+    144,
+    147,
+    238,
+    243,
+    236,
+    240,
+    239,
+    232,
+    237,
+    228
+]
 characters = []
 for val in vals:
-	character = chr((val ^ 220) + 1)
-	characters.append(character) 
+    character = chr((val ^ 220) + 1)
+    characters.append(character)
 
 print("".join(characters))
+
